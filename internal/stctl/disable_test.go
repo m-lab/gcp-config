2acc982db4c8c63b70ff58b63ad24e9bf2d208f5
Stephen Soltesz <soltesz@google.com>
2019-12-19 10:48:27 -0500

    Fix unit tests with expected job

--- internal/stctl/disable_test.go
+++ internal/stctl/disable_test.go
@@ -5,10 +5,24 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/go-test/deep"
 	storagetransfer "google.golang.org/api/storagetransfer/v1"
 )
 
 func TestCommand_Disable(t *testing.T) {
+	expected := &storagetransfer.TransferJob{
+		Name:        "job-name",
+		Description: "This is the job description",
+		Status:      "DISABLED",
+		TransferSpec: &storagetransfer.TransferSpec{
+			GcsDataSource: &storagetransfer.GcsData{
+				BucketName: "source",
+			},
+			GcsDataSink: &storagetransfer.GcsData{
+				BucketName: "destination",
+			},
+		},
+	}
 	tests := []struct {
 		name    string
 		c       *Command
@@ -20,9 +34,17 @@ func TestCommand_Disable(t *testing.T) {
 			c: &Command{
 				Client: &fakeTJ{
 					job: &storagetransfer.TransferJob{
-						Name:        "name",
-						Description: "This is a description",
-						Status:      "OK",
+						Name:        "job-name",
+						Description: "This is the job description",
+						Status:      "NOT-DISABLED",
+						TransferSpec: &storagetransfer.TransferSpec{
+							GcsDataSource: &storagetransfer.GcsData{
+								BucketName: "source",
+							},
+							GcsDataSink: &storagetransfer.GcsData{
+								BucketName: "destination",
+							},
+						},
 					},
 				},
 			},
@@ -54,8 +76,12 @@ func TestCommand_Disable(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			ctx := context.Background()
-			if err := tt.c.Disable(ctx, tt.job); (err != nil) != tt.wantErr {
+			job, err := tt.c.Disable(ctx, tt.job)
+			if (err != nil) != tt.wantErr {
 				t.Errorf("Command.Disable() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if diff := deep.Equal(job, expected); diff != nil && !tt.wantErr {
+				t.Errorf("Command.Disable() job did not match expected; %v", diff)
 			}
 		})
 	}
