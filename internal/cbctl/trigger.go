135ae85d3247a2d1f3bc7b89e6ddd186a4e3000d
Stephen Soltesz <stephen.soltesz@gmail.com>
2021-02-26 12:49:11 -0500

    Update dates

--- internal/cbctl/trigger.go
+++ internal/cbctl/trigger.go
@@ -1,4 +1,4 @@
-// Copyright © 2019 gcp-config Authors.
+// Copyright © 2021 gcp-config Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
